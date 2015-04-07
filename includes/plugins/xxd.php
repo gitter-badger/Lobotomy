a37a71b63918efb78bc703be6f3bfb911b509283
Jeroen Hagebeek <iScripters@gmail.com>
2015-04-07 13:36:52 +0200

    Preparing transition to tabs

--- includes/plugins/xxd.php
+++ includes/plugins/xxd.php
@@ -1,5 +1,4 @@
 <?php
-
 mysql_select_db($_SESSION['dump']['dbase']);
 $query = "SELECT id, filename, fullfilename FROM " . mysql_real_escape_string($_GET['plugin']) . " where id = " . mysql_real_escape_string($_GET['ID']);
 $result = mysql_query($query) or die(mysql_error());
@@ -9,36 +8,45 @@ $result1 = mysql_query($query1);
 echo '<select id="dropdown">' . PHP_EOL;
 $row = mysql_fetch_assoc($result);
 while ($r = mysql_fetch_array($result1)) {
-    if ($r['filename'] != "") {
-        if ($r['filename'] == $row['filename']) {
-            echo '<option value="' . $r['id'] . '" selected="selected"> ' . $r['filename'] . '</option>' . PHP_EOL;
-        } else {
-            echo '<option value="' . $r['id'] . '"> ' . $r['filename'] . '</option>' . PHP_EOL;
-        }
-    }
+if ($r['filename'] != "") {
+if ($r['filename'] == $row['filename']) {
+echo '<option value="' . $r['id'] . '" selected="selected"> ' . $r['filename'] . '</option>' . PHP_EOL;
+} else {
+echo '<option value="' . $r['id'] . '"> ' . $r['filename'] . '</option>' . PHP_EOL;
+}
+}
 }
 echo "</select>";
 
 echo '<code>' . $row['filename'] . '</code>' . PHP_EOL;
-echo '<br /><br />';
-echo '<a href="#strings"><strong>Jump to strings</strong></a>';
-echo '<br />';
-echo '<a name="hex"></a>';
-echo '<pre>';
+?>
+<ul class="tabsB cf">
+<li><a href="#hex">HEX-view</a></li>
+<li><a href="#strings">Strings</a></li>
+</ul>
+<div class="content_panes">
+<div id="hex">
+<pre>
+<?php
 exec('xxd -c 32 ' . $row['fullfilename'], $xxd);
 foreach ($xxd as $line) {
-    echo htmlspecialchars($line).PHP_EOL;
+echo htmlspecialchars($line) . PHP_EOL;
 }
 unset($line);
-echo '</pre>';
-echo '<br /><br />';
-echo '<a href="#hex"><strong>Jump to hex</strong></a>';
-echo '<a name="strings"></a>';
-echo '<pre>';
+?>
+</pre>
+</div>
+<div id="strings">
+<pre>
+<?php
 exec('strings ' . $row['fullfilename'], $strings);
 foreach ($strings as $line) {
-    echo htmlspecialchars($line).PHP_EOL;
+echo htmlspecialchars($line) . PHP_EOL;
 }
-echo '</pre>';
+?>
+</pre>
+</div>
+</div>
+<?php
 mysql_select_db("lobotomy");
 ?>
