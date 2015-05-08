1194727bb0b134813f9ea73560ea5818bcc0c349
iScripters <iScripters@gmail.com>
2015-05-08 10:31:54 +0200

    A session had already been started

--- includes/plugins/netscan.php
+++ includes/plugins/netscan.php
@@ -1,4 +1,3 @@
-<?php session_start(); ?>
 <?php
 mysql_select_db($_SESSION['dump']['dbase']);
 $query = "SELECT `id`, `offset`, `proto`, `localaddress`, `foreignaddress`, `state`, `pid`, `owner`, `createtime` FROM netscan";
