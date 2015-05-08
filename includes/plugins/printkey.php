2e67b12115d3b35299ddbc349ee0fb03dcb9da0b
iScripters <iScripters@gmail.com>
2015-05-08 10:31:36 +0200

    A session had already been started

--- includes/plugins/printkey.php
+++ includes/plugins/printkey.php
@@ -1,4 +1,3 @@
-<?php session_start(); ?>
 <?php
 mysql_select_db($_SESSION['dump']['dbase']);
 $query = "SELECT id, `register`, keyname, keylegend, lastupdated, subkeys, type, `values`, legend, model FROM printkey";
