9eaac3be89149ba92c747b39ada1029c8cd94d3d
iScripters <iScripters@gmail.com>
2015-05-08 09:57:32 +0200

    Test

--- dashboard.php
+++ dashboard.php
@@ -115,7 +115,7 @@ include_once './includes/counters.php';
         <div id="footer">
             <div class="wrapper">
                 <div class="cf ftr_content">
-                    <p class="fl">Copyright &copy; 2015 Wim Venhuizen en Jeroen Hagebeek</p>
+                    <p class="fl">Copyright &copy; 2015 Wim Venhuizen en Jeroen Hagebeek.</p>
                     <a href="javascript:void(0)" class="toTop">Back to top</a>
                 </div>
             </div>
