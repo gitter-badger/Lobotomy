6a499e8d5d9968385db3ef89649976fd852b69f2
Venhuizen <wvenhuizen75@gmail.com>
2015-05-12 19:28:58 +0000

    #27: plugin: photorec: file: PR_file.py PR_dump.py edited online with Bitbucket Fixed

--- PR_dump.py
+++ PR_dump.py
@@ -8,6 +8,11 @@ __author__ = 'Wim Venhuizen, Jeroen Hagebeek'
 # Edited:           W Venhuizen
 # Parsing photorec logfile en bereken md5 over files.
 # kleine aanpassing in console output. geen info is net te weinig als het lang duurt, voor screen maakt output niet uit.
+#
+# Date:             12-05-2015:
+# Edited:           W Venhuizen
+# kleine aanpassing in console output. Bij pct moet het processnaam staan. in de console staat nu alleen pct: ..
+
 
 import sys
 import os
@@ -29,7 +34,7 @@ def main(database):
     pct = 0
 
     Lobotomy.plugin_pct(plugin, database, 0)
-    print "pct done: " + str(pct)
+    print "plugin: " + plugin + " - Database: " + database + " - pct done: " + str(pct)
 
     try:
         log = ""
@@ -56,12 +61,12 @@ def main(database):
     if DEBUG:
         print command
     else:
-        print "Running Photorec. Please wait."
+        print "Running Photorec, database: " + database + ". Please wait."
         log = ""
         status, log = commands.getstatusoutput(command)
 
     Lobotomy.plugin_pct(plugin, database, 25)
-    print "pct done: " + str(25)
+    print "plugin: " + plugin + " - Database: " + database + " - pct done: " + str(25)
 
     if DEBUG:
         print "Write log: (" + casedir + ", Database: " + database + " Stop:  Running PhotoRec: " + plugin + ")"
@@ -91,7 +96,7 @@ def main(database):
                 if not filenaam.endswith("mft"):
                     counter += 1
     Lobotomy.plugin_pct(plugin, database, 30)
-    print "pct done: " + str(30)
+    print "plugin: " + plugin + " - Database: " + database + " - pct done: " + str(30)
     print "Parsing Photorec logfile"
 
     #
@@ -111,26 +116,33 @@ def main(database):
                         filemd5 = Lobotomy.md5Checksum(filenaam)
                     except:
                         pass
-                    pct = str(float(1.0 * count / counter) * 25).split(".")[0]
+                    pct = str(31 + (float(1.0 * count / counter) * 25)).split(".")[0]
+
                     filename = filenaam.split("/")[-1]
-                    SQL_cmd = "INSERT INTO PR_files VALUES (0, '{}', '{}', '{}')".format(filenaam, filename, filemd5)
-                    if DEBUG:
-                        print SQL_cmd
-                    else:
-                        Lobotomy.exec_sql_query(SQL_cmd, database)
-                        if pct != pcttmp:
-                            print "pct done: " + str(pct)
-                            Lobotomy.plugin_pct(plugin, database, pct)
+                    try:
+                        SQL_cmd = "INSERT INTO PR_files VALUES (0, '{}', '{}', '{}')".format(filenaam, filename, filemd5)
+                    except:
+                        pass #UnboundLocalError: local variable 'filemd5' referenced before assignment
+                    try:
+                        if DEBUG:
+                            print SQL_cmd
+                        else:
+                            Lobotomy.exec_sql_query(SQL_cmd, database)
+                            if pct != pcttmp:
+                                print "plugin: " + plugin + " - Database: " + database + " - pct done: " + str(pct)
+                                Lobotomy.plugin_pct(plugin, database, pct)
+                    except:
+                        pass
                     pcttmp = pct
 
 
 
     Lobotomy.plugin_pct(plugin, database, 50)
-    print "pct done: " + str(50)
+    print "plugin: " + plugin + " - Database: " + database + " - pct done: " + str(50)
     # bereken md5 en sha1 hash over de files uit reports.log
     # exif info over de files
     Lobotomy.plugin_pct(plugin, database, 100)
-    print "pct done: " + str(100)
+    print "plugin: " + plugin + " - Database: " + database + " - pct done: " + str(100)
     
 if __name__ == "__main__":
     if len(sys.argv) != 2:
