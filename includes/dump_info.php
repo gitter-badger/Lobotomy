2eecfebf14ab85e97e22cd680dc0002ed88a8b99
iScripters <iScripters@gmail.com>
2015-05-13 21:22:54 +0200

    Only retrieve dump info if dump is selected. Excluded database connection, should already exist

--- includes/dump_info.php
+++ includes/dump_info.php
@@ -1,15 +1,17 @@
 <?php
-include_once './includes/database.php';
-$query = "SELECT id, location, dbase, added, case_assigned FROM dumps WHERE id=" . $_SESSION['dump']['id'];
-$result = mysqli_query($sqldb, $query);
-$row = mysqli_fetch_assoc($result);
+//include_once './includes/database.php';
+if (isset($_SESSION['dump']['id'])) {
+    $query = "SELECT id, location, dbase, added, case_assigned FROM dumps WHERE id=" . $_SESSION['dump']['id'];
+    $result = mysqli_query($sqldb, $query);
+    $row = mysqli_fetch_assoc($result);
 
-$dump['id'] = $row['id'];
-$dump['location'] = $row['location'];
-$dump['dbase'] = $row['dbase'];
-$dump['added'] = $row['added'];
-$dump['case_assigned'] = $row['case_assigned'];
+    $dump['id'] = $row['id'];
+    $dump['location'] = $row['location'];
+    $dump['dbase'] = $row['dbase'];
+    $dump['added'] = $row['added'];
+    $dump['case_assigned'] = $row['case_assigned'];
 
-$_SESSION['dump'] = $dump;
-$_SESSION['dump']['selected'] = True;
+    $_SESSION['dump'] = $dump;
+    $_SESSION['dump']['selected'] = True;
+}
 ?>
