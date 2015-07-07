f3228fe23b5150862feea6dcce50afeb492a3aa8
Venhuizen <wvenhuizen75@gmail.com>
2015-07-07 15:51:43 +0200

    d

--- lob_zookeeper.py
+++ lob_zookeeper.py
@@ -39,53 +39,105 @@ def main(database, folder):
     print "Please wait..\nRunning Zookeeper script on folder: " + folder
     log = ""
     status, log = commands.getstatusoutput(command)
-    counter = 0
     sql_prefix = "INSERT INTO pe_scan VALUES (0, "
     sql_line = ''
     items = log.split('\n')
     print 'Parsing Zookeeper data'
     for item in items:
-        print type(item)
         if item.startswith('fullfilename'):
-            fullfilename = item.split(' ', 1)[1]
+            try:
+                fullfilename = item.split(' ', 1)[1]
+            except:
+                pass
         if item.startswith('pe_compiletime'):
-            pe_compiletime = item.split(' ', 1)[1]
+            try:
+                pe_compiletime = item.split(' ', 1)[1]
+            except:
+                pass
         if item.startswith('original_filename'):
-            original_filename = item.split(' ', 1)[1]
+            try:
+                original_filename = item.split(' ', 1)[1]
+            except:
+                pass
         if item.startswith('pe_packer'):
-            pe_packer = item.split(' ', 1)[1]
+            try:
+                pe_packer = item.split(' ', 1)[1]
+            except:
+                pass
         if item.startswith('filetype'):
-            filetype = item.split(' ', 1)[1]
+            try:
+                filetype = item.split(' ', 1)[1]
+            except:
+                pass
         if item.startswith('pehash'):
-            pehash = item.split(' ', 1)[1]
+            try:
+                pehash = item.split(' ', 1)[1]
+            except:
+                pass
         if item.startswith('md5'):
-            md5 = item.split(' ', 1)[1]
+            try:
+                md5 = item.split(' ', 1)[1]
+            except:
+                pass
         if item.startswith('pe_language'):
-            pe_language = item.split(' ', 1)[1]
+            try:
+                pe_language = item.split(' ', 1)[1]
+            except:
+                pass
         if item.startswith('pe_dll'):
-            pe_dll = item.split(' ', 1)[1]
+            try:
+                pe_dll = item.split(' ', 1)[1]
+            except:
+                pass
         if item.startswith('filename'):
-            filename = item.split(' ', 1)[1]
+            try:
+             filename = item.split(' ', 1)[1]
+            except:
+                pass
         if item.startswith('sha'):
-            sha = item.split(' ', 1)[1]
+            try:
+                sha = item.split(' ', 1)[1]
+            except:
+                pass
         if item.startswith('tag'):
-            tag = item.split(' ', 1)[1]
+            try:
+                tag = item.split(' ', 1)[1]
+                sql = 1
+            except:
+                pass
         if item.startswith('filesize'):
-            filesize = item.split(' ', 1)[1]
+            try:
+                filesize = item.split(' ', 1)[1]
+            except:
+                pass
         if item.startswith('yara_results'):
-            yara_results = item.split(' ', 1)[1]
-        if counter == 1:
+            try:
+                yara_results = item.split(' ', 1)[1]
+            except:
+                pass
+        if item.startswith('*****'):
             sql_line = sql_prefix + "'{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}'".\
                 format(fullfilename, original_filename, pe_compiletime, pe_packer, filetype, pe_language, pe_dll,\
                 filename, md5, sha, pehash, tag, filesize, yara_results)
-        if item.startswith('*****'):
-            counter = 1
             print sql_line
             sql_line = ''
+            fullfilename = ''
+            pe_compiletime = ''
+            original_filename = ''
+            pe_packer = ''
+            filetype = ''
+            pehash = ''
+            md5 = ''
+            pe_language = ''
+            pe_dll = ''
+            filename = ''
+            sha = ''
+            tag = ''
+            filesize = ''
+            yara_results = ''
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print "Usage: " + plugin + ".py <database> <filename>"
     else:
         main(sys.argv[1], sys.argv[2])
-
